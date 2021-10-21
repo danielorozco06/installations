c6055f97e7b146508080ca5c2fcd04c970130ad9
danielorozco06 <danielorozcomoya@gmail.com>
2021-10-21 08:41:03 -0500

    Add a commment

--- setWslConf.sh
+++ setWslConf.sh
@@ -3,6 +3,8 @@
 echo -e "\nsudo rm -f /etc/wsl.conf"
 sudo rm -f /etc/wsl.conf
 
+#Important: This is needed to execute an import .tar like default user, and no like root
+
 echo -e ""
 echo '[user]' | sudo tee -a /etc/wsl.conf
 echo "default=$USER" | sudo tee -a /etc/wsl.conf
